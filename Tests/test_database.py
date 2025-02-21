98e4a6ba7d27c660ea1667364ee5cd46fa6b7a17
bagelquant <eric.yanzhong.huang@gmail.com>
2025-02-21 09:38:01 -0500

    [0.2.0] refactor database module
    - Refactoring database base class, refer to v_0_2_0.md doc
    - Adding SQLite child class

--- Tests/test_database.py
+++ Tests/test_database.py
@@ -4,12 +4,12 @@ Tests for mysql database
 Author: Yanzhong Huang
 Email: bagelquant@gmail.com
 """
-
 import json
 import os
+import unittest
 from unittest import TestCase
 
-from src.bageltushare.database import MySQL
+from src.bageltushare.database import MySQL, SQLite
 from sqlalchemy import text
 
 
@@ -17,16 +17,67 @@ class TestMySQLDB(TestCase):
 
     def setUp(self):
         """initialize mysql db"""
-        with open(os.path.join(os.path.dirname(__file__), 'test_config.json')) as f:
-            config = json.load(f)['database_config']
+        with open(os.path.join(os.path.dirname(__file__), "test_config.json")) as f:
+            config = json.load(f)["database_config"]
 
         self.db = MySQL(**config)
+        
 
-    def test_conenction(self):
+    def _create_test_table(self):
+        # create a test table
         with self.db.engine.begin() as conn:
-            sql = text('SHOW TABLES')
-            result = conn.execute(sql)
-            print([row for row in result])
+            sql = text("CREATE TABLE IF NOT EXISTS test_table ("
+                       "id INT AUTO_INCREMENT PRIMARY KEY,"
+                       "trade_date DATE NOT NULL,"
+                       "name VARCHAR(255) NOT NULL,"
+                       "age INT NOT NULL"
+                       ")")
+            conn.execute(sql)
+
+    def _drop_test_table(self):
+        with self.db.engine.begin() as conn:
+            sql = text("DROP TABLE IF EXISTS test_table")
+            conn.execute(sql)
 
     def test_create_index(self):
-        self.db.create_index('stock_basic')
+        self._create_test_table()
+        self.db.create_index("test_table")
+        self._drop_test_table()
+        print("MySQL index creation test passed")
+
+
+class TestSQLite(TestCase):
+
+    def setUp(self):
+        """initialize sqlite db"""
+        path: str = os.path.join(os.path.dirname(__file__), "test.db")
+        self.db = SQLite(path=path)
+
+    def _create_test_table(self):
+        # create a test table
+        with self.db.engine.begin() as conn:
+            sql = text("CREATE TABLE IF NOT EXISTS test_table ("
+                       "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                       "trade_date DATE NOT NULL,"
+                       "name VARCHAR(255) NOT NULL,"
+                       "age INT NOT NULL"
+                       ")")
+            conn.execute(sql)
+
+    def _drop_test_table(self):
+        with self.db.engine.begin() as conn:
+            sql = text("DROP TABLE IF EXISTS test_table")
+            conn.execute(sql)
+
+    def test_create_index(self):
+        self._create_test_table()
+        self.db.create_index("test_table")
+        self._drop_test_table()
+        print("SQLite index creation test passed")
+
+    def test_remove_db(self):
+        path: str = os.path.join(os.path.dirname(__file__), "test.db")
+        os.remove(path)
+
+if __name__ == "__main__":
+    unittest.main()
--- src/bageltushare/database.py
+++ src/bageltushare/database.py
@@ -5,54 +5,100 @@ Author: Yanzhong Huang
 Email: bagelquant@gmail.com
 """
 
+from dataclasses import dataclass, field
 from sqlalchemy import create_engine, Engine, text
 from abc import ABC, abstractmethod
 
 
+@dataclass(slots=True)
 class Database(ABC):
-    """database base"""
+
+    engine: Engine = field(init=False)
+
+    def __post_init__(self):
+        self._create_engine()
 
     @abstractmethod
-    def get_engine(self) -> Engine:
-        """get a sqlalchemy engine"""
+    def _create_engine(self) -> None:
         ...
 
     @abstractmethod
     def create_index(self, table_name: str) -> None:
-        """create index for all table, index columns: ts_code, trade_date, f_ann_date"""
+        """
+        Create index for {table_name} if not exists:
+        - ts_code
+        - trade_date
+        - f_ann_date
+        """
+        ...
+
+    @abstractmethod
+    def _check_index_exists(self, table_name: str) -> bool:
         ...
 
 
+@dataclass(slots=True)
 class MySQL(Database):
-    """
-    MySQL connection
-    """
 
-    def __init__(self, host, port, user, password, database):
-        self.host = host
-        self.port = port
-        self.user = user
-        self.password = password
-        self.database = database
-        self.engine = self._create_engine()
+    host: str
+    port: int
+    user: str
+    password: str
+    database: str
 
-    def _create_engine(self):
-        return create_engine(
-            f'mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}'
+    def _create_engine(self) -> None:
+        self.engine = create_engine(
+            f"mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
         )
 
-    def get_engine(self) -> Engine:
-        return self.engine
+    def _check_index_exists(self, table_name: str) -> bool:
+        with self.engine.begin() as conn:
+            result = conn.execute(text(f"SHOW INDEX FROM {table_name}"))
+            return result.rowcount > 0
 
     def create_index(self, table_name: str) -> None:
-        with self.engine.begin() as con:
+        # check if index exists
+        if self._check_index_exists(table_name):
+            return
+        with self.engine.begin() as conn:
             # query columns names for the table
-            result = con.execute(text(f'SHOW COLUMNS FROM {table_name}'))
+            result = conn.execute(text(f"SHOW COLUMNS FROM {table_name}"))
             columns = [row[0] for row in result]
 
-            if 'ts_code' in columns:
-                con.execute(text(f'CREATE INDEX idx_{table_name}_ts_code ON {table_name}(ts_code(10))'))
-            if 'trade_date' in columns:
-                con.execute(text(f'CREATE INDEX idx_{table_name}_trade_date ON {table_name}(trade_date)'))
-            if 'f_ann_date' in columns:
-                con.execute(text(f'CREATE INDEX idx_{table_name}_f_ann_date ON {table_name}(f_ann_date)'))
+            if "ts_code" in columns:
+                conn.execute(text(f"CREATE INDEX idx_{table_name}_ts_code ON {table_name}(ts_code(10))"))
+            if "trade_date" in columns:
+                conn.execute(text(f"CREATE INDEX idx_{table_name}_trade_date ON {table_name}(trade_date)"))
+            if "f_ann_date" in columns:
+                conn.execute(text(f"CREATE INDEX idx_{table_name}_f_ann_date ON {table_name}(f_ann_date)"))
+
+
+@dataclass(slots=True)
+class SQLite(Database):
+
+    path: str
+
+    def _create_engine(self) -> None:
+        self.engine = create_engine(f"sqlite:///{self.path}")
+
+    def _check_index_exists(self, table_name: str) -> bool:
+        with self.engine.begin() as conn:
+            result = conn.execute(text(f"PRAGMA index_list({table_name})"))
+            return result.rowcount > 0
+
+    def create_index(self, table_name: str) -> None:
+        # check if index exists
+        if self._check_index_exists(table_name):
+            return
+        with self.engine.begin() as conn:
+            # query columns names for the table
+            result = conn.execute(text(f"PRAGMA table_info({table_name})"))
+            columns = [row[1] for row in result]
+
+            if "ts_code" in columns:
+                conn.execute(text(f"CREATE INDEX idx_{table_name}_ts_code ON {table_name}(ts_code)"))
+            if "trade_date" in columns:
+                conn.execute(text(f"CREATE INDEX idx_{table_name}_trade_date ON {table_name}(trade_date)"))
+            if "f_ann_date" in columns:
+                conn.execute(text(f"CREATE INDEX idx_{table_name}_f_ann_date ON {table_name}(f_ann_date)"))
+
